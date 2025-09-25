fad2e0df80b70486a7e1c1b00c1272ee9cbb504c
Harsh Kashyap <harshkashyap.hk996@gmail.com>
2025-09-25 18:07:52 +0000

    added workflow for pushing image to GHCR

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-def add(x, y):
-    return x + y
+#def add(x, y):
+#    return x + y
+#
+
+def main():
+    print("Hello from inside the Docker container!")
+
+if __name__ == "__main__":
+    main()
